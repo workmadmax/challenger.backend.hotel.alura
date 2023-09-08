e0705a65a893a90d95e78a614bb94c7636d14c41
mdouglas <work.madmax@gmail.com>
2023-09-08 19:03:08 -0300

    fix: return method for response entity and http code return

--- hotel-api/src/main/java/com/challenge/hotel/alura/hotelapi/controllers/UserController.java
+++ hotel-api/src/main/java/com/challenge/hotel/alura/hotelapi/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.challenge.hotel.alura.hotelapi.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DataListUsers;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DataRegisterUser;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DataUpdateUsers;
+import com.challenge.hotel.alura.hotelapi.domain.user_system.DetailUser;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.UserRepository;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.UserSystem;
 
@@ -33,29 +36,42 @@ public class UserController {
 
     @PostMapping
     @Transactional
-    public void registerUser(@RequestBody @Valid DataRegisterUser data) {
+    public ResponseEntity<DetailUser> registerUser(@RequestBody @Valid DataRegisterUser data, UriComponentsBuilder uriBuilder) {
         
-        userRepository.save(new UserSystem(data));
+        var user = new UserSystem(data);
+        userRepository.save(user);
+        var uri = uriBuilder.path("/hotel_alura/users_system/{id}").buildAndExpand(user.getId()).toUri();
+        return (ResponseEntity.created(uri).body(new DetailUser(user)));
     }
 
     @GetMapping
-    public Page<DataListUsers> listUsers(Pageable pagination) {
-        return userRepository.findAllByActiveTrue(pagination).map(DataListUsers::new);
+    public ResponseEntity<Page<DataListUsers>> listUsers(Pageable pagination) {
+        var page = userRepository.findAllByActiveTrue(pagination).map(DataListUsers::new);
+        return (ResponseEntity.ok(page));
+
     }
     
     @PutMapping
     @Transactional
-    public void toUpdate(@RequestBody @Valid DataUpdateUsers data) {
+    public ResponseEntity<DetailUser> toUpdate(@RequestBody @Valid DataUpdateUsers data) {
         
         var user = userRepository.getReferenceById(data.id());
         user.updateData(data);
+        return (ResponseEntity.ok(new DetailUser(user)));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         var user = userRepository.getReferenceById(id);
         user.setActive(false);
+
+        return (ResponseEntity.noContent().build());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<DetailUser> showUserDetails(@PathVariable Long id) {
+        var user = userRepository.getReferenceById(id);
+        return (ResponseEntity.ok(new DetailUser(user)));
+    }
 }
