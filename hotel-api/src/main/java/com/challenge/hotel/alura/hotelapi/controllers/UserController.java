40c1748ba47b13acc49e2697aebe139e90e869f4
mdouglas <work.madmax@gmail.com>
2023-09-08 15:37:59 -0300

    fix: User conroler

--- hotel-api/src/main/java/com/challenge/hotel/alura/hotelapi/controllers/UserController.java
+++ hotel-api/src/main/java/com/challenge/hotel/alura/hotelapi/controllers/UserController.java
@@ -1,15 +1,23 @@
 package com.challenge.hotel.alura.hotelapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.challenge.hotel.alura.hotelapi.domain.user_system.DataListUsers;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DataRegisterUser;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.UserRepository;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.UserSystem;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/hotel_alura/users_system")
@@ -18,11 +26,19 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+
+    
+
     @PostMapping
     @Transactional
-    public void registerUser(@RequestBody DataRegisterUser data) {
+    public void registerUser(@RequestBody @Valid DataRegisterUser data) {
         
         userRepository.save(new UserSystem(data));
+    }
+
+    @GetMapping
+    public Page<DataListUsers> listUsers(Pageable pagination) {
+        return userRepository.findAll(pagination).map(DataListUsers::new);
     }
     
 }
