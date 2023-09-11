124c26bea0940706ff5d3dbb97aaba33a16095d9
mdouglas <work.madmax@gmail.com>
2023-09-11 16:32:31 -0300

    fix user controller

--- hotel-api/src/main/java/com/challenge/hotel/alura/hotelapi/controllers/UserController.java
+++ hotel-api/src/main/java/com/challenge/hotel/alura/hotelapi/controllers/UserController.java
@@ -5,70 +5,61 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DataListUsers;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DataRegisterUser;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DataUpdateUsers;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.DetailUser;
-import com.challenge.hotel.alura.hotelapi.domain.user_system.UserRepository;
 import com.challenge.hotel.alura.hotelapi.domain.user_system.UserSystem;
+import com.challenge.hotel.alura.hotelapi.services.UserService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/hotel_alura/users_system")
 public class UserController {
 
+    private final UserService userService;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping
     @Transactional
     public ResponseEntity<DetailUser> registerUser(@RequestBody @Valid DataRegisterUser data, UriComponentsBuilder uriBuilder) {
-        
-        var user = new UserSystem(data);
-        userRepository.save(user);
+        UserSystem user = userService.registerUser(data);
         var uri = uriBuilder.path("/hotel_alura/users_system/{id}").buildAndExpand(user.getId()).toUri();
-        return (ResponseEntity.created(uri).body(new DetailUser(user)));
+        return ResponseEntity.created(uri).body(new DetailUser(user));
     }
 
     @GetMapping
     public ResponseEntity<Page<DataListUsers>> listUsers(Pageable pagination) {
-        var page = userRepository.findAllByActiveTrue(pagination).map(DataListUsers::new);
-        return (ResponseEntity.ok(page));
-
+        Page<DataListUsers> page = userService.listUsers(pagination);
+        return ResponseEntity.ok(page);
     }
-    
+
     @PutMapping
     @Transactional
     public ResponseEntity<DetailUser> toUpdate(@RequestBody @Valid DataUpdateUsers data) {
-        
-        var user = userRepository.getReferenceById(data.id());
-        user.updateData(data);
-        return (ResponseEntity.ok(new DetailUser(user)));
+        UserSystem user = userService.updateUser(data);
+        return ResponseEntity.ok(new DetailUser(user));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
-        var user = userRepository.getReferenceById(id);
-        user.setActive(false);
-
-        return (ResponseEntity.noContent().build());
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<DetailUser> showUserDetails(@PathVariable Long id) {
-        var user = userRepository.getReferenceById(id);
-        return (ResponseEntity.ok(new DetailUser(user)));
+        UserSystem user = userService.getUserById(id);
+        return ResponseEntity.ok(new DetailUser(user));
     }
 }
